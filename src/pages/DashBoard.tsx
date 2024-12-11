f198abe20c0c89aa8c0842d6a8eecdd890d692fa
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-12-11 21:36:29 +0530

    dashboard modified and notification data changed

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
-import OnChainData from "./OnChainData";
+//import OnChainData from "./OnChainData";
 import CvById from "./CvById";
-import { connectWallet, getContract } from "@/api/contract.api";
+import { connectWallet } from "@/api/contract.api";
 import toast from "react-hot-toast";
 import { useCvFromContext } from "@/context/CvForm.context";
 import { API_BASE_URL } from "@/main";
@@ -12,26 +12,26 @@ import NFTGallery from "./NFTData";
 const DashBoard = () => {
     const [isActiveButton , setActiveButton] = useState<boolean>(true);
     const {account,setAccount} = useCvFromContext();
-    const [docData, setDocData] = useState([]);
+ //   const [docData, setDocData] = useState([]);
     const [cvData, setCvData] = useState([]);
     const [isNFT, setNFT] = useState<boolean>(false);
 
-    const getDoc = async()=>{
-      try{
-      const contract = await getContract();
-      const tx = await contract?.getStudentHashes();
-      console.log("tx",tx)
-      setDocData(tx);
-      if(tx.length===0)
-      {
-        toast.error("No certificate available")
-      }
-    }catch(err)
-    {
-      toast.error("something went wrong");
-      console.log(err);
-    }
-     }
+    // const getDoc = async()=>{
+    //   try{
+    //   const contract = await getContract();
+    //   const tx = await contract?.getStudentHashes();
+    //   console.log("tx",tx)
+    //   setDocData(tx);
+    //   if(tx.length===0)
+    //   {
+    //     toast.error("No certificate available")
+    //   }
+    // }catch(err)
+    // {
+    //   toast.error("something went wrong");
+    //   console.log(err);
+    // }
+    //  }
 
      const getAccount = async()=>{
       try
@@ -46,10 +46,10 @@ const DashBoard = () => {
       }
      }
 
-     const fetchDataHandler=()=>{
-      setActiveButton(false);
-      getDoc();
-     }
+    //  const fetchDataHandler=()=>{
+    //   setActiveButton(false);
+    //   getDoc();
+    //  }
 
      const fetchIds = async()=>{
       const id=toast.loading("document fetching...")
@@ -80,7 +80,12 @@ const DashBoard = () => {
 
      const idFetchHandler = ()=>{
       setActiveButton(true)
+      setNFT(false);
       fetchIds();
+     }
+     const nftFetchHandler = ()=>{
+      setActiveButton(false)
+      setNFT(true);
      }
 
   return (
@@ -90,15 +95,15 @@ const DashBoard = () => {
         {!account?
         <Button type="button" onClick={getAccount}>Connect Wallet</Button>:
         <>
-        <Button className={`text-center border border-slate-300 text-[#006666] hover:bg-slate-100 ${!isActiveButton?"bg-slate-100":"bg-white border"}`} onClick={fetchDataHandler}>Fetch your on-chain documents</Button>
-        <Button className={`text-center border border-slate-300 text-[#006666] hover:bg-slate-100 ${            isNFT?"bg-slate-100":"bg-white border"}`} onClick={()=>setNFT(true)}>Fetch your NFTs</Button>
+        {/* <Button className={`text-center border border-slate-300 text-[#006666] hover:bg-slate-100 ${!isActiveButton?"bg-slate-100":"bg-white border"}`} onClick={fetchDataHandler}>Fetch your on-chain documents</Button> */}
+        <Button className={`text-center border border-slate-300 text-[#006666] hover:bg-slate-100 ${            isNFT?"bg-slate-100":"bg-white border"}`} onClick={nftFetchHandler}>Fetch your NFTs</Button>
         </>
 }
         </div>
         <div className="flex justify-center items-center gap-2 h-screen">
-          {
+          {/* {
             !isActiveButton&&<OnChainData docData={docData}/>
-          }
+          } */}
           {
             isActiveButton&&<CvById cvData={cvData}/>
           }
